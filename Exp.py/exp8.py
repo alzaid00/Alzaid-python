a2831b726d352ff183f5c7288db380229d912355
alzaid00 <alzaidkhan00@eng.rizvi.edu.in>
2025-04-24 00:49:56 +0530

    Create exp8.py

---
+++ Exp.py/exp8.py
@@ -0,0 +1,14 @@
+
+# Program 7: Even or Odd Number Check
+# Name: Alzaid khan  DIV: E(ECS)
+# UIN: 241S009  Roll no. 
+
+#Program 7: Even or Odd Number Check
+def check_even_odd():
+    num = int(input("Enter a number: "))
+    if num % 2 == 0:
+        print(f"{num} is Even.")
+    else:
+        print(f"{num} is Odd.")
+
+check_even_odd()
