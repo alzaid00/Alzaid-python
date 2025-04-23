1c8bccbae684dfcf014ea81e7b9e9c8db4f7d618
alzaid00 <alzaidkhan00@eng.rizvi.edu.in>
2025-04-23 23:56:57 +0530

    exp 2.py

---
+++ exp.py
@@ -0,0 +1,17 @@
+#Program 1: Calculate Areas of Geometric Figures
+# Circle area
+r = float(input("Enter radius of circle: "))
+circle_area = 3.14 * r * r
+print("Area of Circle =", circle_area)
+
+# Rectangle area
+l = float(input("Enter length of rectangle: "))
+b = float(input("Enter breadth of rectangle: "))
+rectangle_area = l * b
+print("Area of Rectangle =", rectangle_area)
+
+# Triangle area
+base = float(input("Enter base of triangle: "))
+height = float(input("Enter height of triangle: "))
+triangle_area = 0.5 * base * height
+print("Area of Triangle =", triangle_area)
