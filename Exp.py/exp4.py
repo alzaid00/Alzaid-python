e8315604f71a36391f721412719456e1e60110eb
alzaid00 <alzaidkhan00@eng.rizvi.edu.in>
2025-04-24 00:17:30 +0530

    Create exp4.py

---
+++ Exp.py/exp4.py
@@ -0,0 +1,11 @@
+#Program 3: Arithmetic Operations
+def arithmetic_ops():
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+    print(f"Addition: {a + b}")
+    print(f"Subtraction: {a - b}")
+    print(f"Multiplication: {a * b}")
+    print(f"Division: {a / b}")
+    print(f"Modulus: {a % b}")
+
+arithmetic_ops()
